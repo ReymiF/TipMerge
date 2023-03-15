4d272054617225902428aacbf501e9074fa348e1
ReymiF <remiktb@gmail.com>
2023-03-15 17:09:22 +0100

    Initial commit

---
+++ src/main/java/com/reymi/tipmerge/TipmergeApplication.java
@@ -0,0 +1,13 @@
+package com.reymi.tipmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipmergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipmergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reymi/tipmerge/TipmergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reymi.tipmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipmergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
